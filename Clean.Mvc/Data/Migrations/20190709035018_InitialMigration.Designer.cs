1d2adc1cea0a719c748c4d96014dd491186d5a2c
Administrator <Administrator@CITADEL-9>
2019-07-09 12:53:10 +0530

    Project Structure Implementation

--- Clean.Mvc/Data/Migrations/20190709035018_InitialMigration.Designer.cs
+++ Clean.Mvc/Data/Migrations/20190709035018_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using Clean.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Clean.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190709035018_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
